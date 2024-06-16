397635fc580875789b59415f09c88990c043fe3d
mehmetb <mehmet3bulam@gmail.com>
2024-06-16 16:53:55 +0300

    feat: tanstack/react-query library added

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,8 +1,14 @@
 "use client";
 
+import { queryClient } from "@/utils/api";
 import { chakraUiTheme } from "@/utils/theme";
 import { ChakraProvider } from "@chakra-ui/react";
+import { QueryClientProvider } from "@tanstack/react-query";
 
 export function Providers({ children }: { children: React.ReactNode }) {
-  return <ChakraProvider theme={chakraUiTheme}>{children}</ChakraProvider>;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ChakraProvider theme={chakraUiTheme}>{children}</ChakraProvider>
+    </QueryClientProvider>
+  );
 }
